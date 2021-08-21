8eacea674edd60ae3f8221503ffbc5e6781582d9
Stéphane Conq <conqstephane@gmail.com>
2021-08-21 20:12:07 +0200

    fixed interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,4 @@
 
-import axios from 'axios'
-
-axios.interceptors.request.use((config) => {
-  if (config.url.startsWith('/')) {
-    config.url = process.env.VUE_APP_API_URL + config.url
-  }
-  if (process.env.NODE_ENV === 'local') {
-    config.headers['x-goog-authenticated-user-email'] = process.env.VUE_APP_IAP_USER
-  }
-  return config
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error)
-})
+export default function ({ $axios }) {
+  $axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+}
