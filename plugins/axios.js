46bcd0b319e02bc21c038bcdaea841effb1b9707
Stéphane Conq <conqstephane@gmail.com>
2021-08-21 14:23:00 +0200

    corrected interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,15 @@
 
-export default function ({ $axios }) {
-  $axios.defaults.baseURL = process.env.API_URL;
-}
+import axios from 'axios'
+
+axios.interceptors.request.use((config) => {
+  if (config.url.startsWith('/')) {
+    config.url = process.env.API_URL + config.url
+  }
+  if (process.env.NODE_ENV === 'local') {
+    config.headers['x-goog-authenticated-user-email'] = process.env.VUE_APP_IAP_USER
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
