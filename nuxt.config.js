86ba0c1f902a4b24c54d11aa40493251c9ca1b97
Stéphane Conq <conqstephane@gmail.com>
2021-08-21 20:22:39 +0200

    corrected nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  generate: {
+    fallback: false,
+    routes: ['/', '/recette', '/carton', '/betise', '/couture']
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Bepedia',
